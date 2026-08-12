d5545049060aa8a3f32e2bca56aaa29d8b4536f6
GuilhermeSantana <guilhermebragasantana@gmail.com>
2026-08-11 22:29:10 -0300

    terminei todo o design mobile, e comecei a mexer no JS dos destinos

--- js/destinos.js
+++ js/destinos.js
@@ -1,3 +1,13 @@
-import { destinationData } from './DestinationData';
+import { destinationData } from './destinationData.js';
+
+
+const btn = document.querySelectorAll('.destinationButton');
+
+btn.forEach(function(button){
+    button.addEventListener('click', function() {
+        console.log(button.dataset.destinationData)
+    })
+})
+
 
 let currentDestination = 'itacare';
