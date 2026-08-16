28cc0356027f0c0fe1e9c064befb0ace4cb45e79
GuilhermeSantana <guilhermebragasantana@gmail.com>
2026-08-16 18:39:14 -0300

    Mudei algumas coisas no HTML, e começei o JS da página.

--- js/destinationData.js
+++ js/destinationData.js
@@ -4,22 +4,22 @@ const destinationData = {
         description: "Itacaré é um município no litoral sul da Bahia, famoso pelas praias de surf, trilhas na Mata Atlântica e cachoeiras próximas. Tem um clima jovem e rústico-chique, com pousadas charmosas e boa gastronomia.",
         history: "Fundada por jesuítas no século XVI/XVIII como São Miguel da Barra do Rio de Contas, Itacaré viveu do cacau até o declínio nos anos 1980 com a praga da vassoura-de-bruxa. O isolamento preservou a natureza, e a cidade renascceu turisticamente após a construção da estrada Ilhéus-Itacaré em 1998.",
         photos: [
-            {src: "img/Desing/fundo-cinza-claro_926199-503748.avif", caption: "Exemplo"},
-            {src: "img/Desing/fundo-cinza-claro_926199-503748.avif", caption: "Exemplo"}
+            {src: "img/cinza.webp", caption: "Exemplo"},
+            {src: "img/cinza.webp", caption: "Exemplo"}
         ]
     },
 
-    MorroDeSaoPaulo: {
+    morroDeSaoPaulo: {
         name: "Morro de São Paulo",
         description: "Morro de São Paulo é uma vila na Bahia, acessível só de barco, famosa por suas praias numeradas, águas claras e vida noturna animada.",
         history: "Descoberta em 1531, a vila foi fundada em 1535, teve papel de defesa colonial e foi atacada na Segunda Guerra. Só nos anos 70, com a chegada de hippies, deixou de ser um vilarejo isolado de pescadores e virou o polo turístico atual.",
         photos: [
-            {src: "img/Desing/fundo-cinza-claro_926199-503748.avif", caption: "Exemplo"},
-            {src: "img/Desing/fundo-cinza-claro_926199-503748.avif", caption: "Exemplo"}
+            {src: "img/cinza.webp", caption: "Exemplo"},
+            {src: "img/cinza.webp", caption: "Exemplo"}
         ]
     },
 
-    BarraGrande: {
+    barraGrande: {
         name: "Barra Grande",
         description: "Barra Grande é um vilarejo na Península de Maraú, litoral sul da Bahia, banhado pela Baía de Camamu, famoso por suas praias, piscinas naturais e clima tranquilo de ex-vila de pescadores.",
         history: "Ligada à origem de Maraú, fundada em 1705 por frades capuchinhos italianos numa antiga aldeia indígena chamada Mayrahú. A região virou vila em 1761 e cidade em 1938. Barra Grande, antiga vila de pescadores, tornou-se hoje o principal destino turístico da península.",
--- js/destinos.js
+++ js/destinos.js
@@ -1,13 +1,19 @@
 import { destinationData } from './destinationData.js';
 
-
 const btn = document.querySelectorAll('.destinationButton');
 
 btn.forEach(function(button){
     button.addEventListener('click', function() {
-        console.log(button.dataset.destinationData)
+        //I kept the name "botão" in Portuguese to make it easier to read.
+        btn.forEach(function(botao){
+            botao.classList.remove('active')
+        })
+        button.classList.add('active');
+        console.log(button.dataset.destination)
+        const destination = button.dataset.destination;
+        const data = destinationData[destination];
+        document.getElementById('nameDestiny').innerHTML = data.name;
+        document.getElementById('historyDestiny').textContent = data.description;
+        document.getElementById('imgDestiny-first') = style.backgroung-color : yellow;
     })
 })
-
-
-let currentDestination = 'itacare';
